a8db8eec7fb868984a95b41d790b06b652affa78
Kiran <krnsundal@gmail.com>
2023-08-01 15:47:58 +0100

    Candladora project added to this repo

---
+++ frontend/src/components/Home/Slider/Slider.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./slider.css";
+
+const Slider = () => {
+  return (
+    <>
+      <slider>
+        <slide></slide>
+        <slide></slide>
+        <slide></slide>
+      </slider>
+    </>
+  );
+};
+
+export default Slider;
